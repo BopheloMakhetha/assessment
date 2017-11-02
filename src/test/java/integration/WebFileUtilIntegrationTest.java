1b9881d286834bd55b79db778b0908a0f3adc647
Bophelo <bophelo.mkhwanazi@dariel.co.za>
2017-11-02 10:15:48 +0200

    integration tests for downloading html from link

---
+++ src/test/java/integration/WebFileUtilIntegrationTest.java
@@ -0,0 +1,24 @@
+package integration;
+
+import domain.WebFile;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import util.WebFileUtil;
+
+@Category(WebFileUtil.class)
+public class WebFileUtilIntegrationTest {
+
+    String url = "https://github.com/egis/handbook/blob/master/Tech-Stack.md";
+
+    @Test
+    public void testDownloadSourceFromLink() throws Exception {
+        WebFileUtil webFileUtil = new WebFileUtil();
+        WebFile webFile = new WebFile(url);
+        webFileUtil.populateWebfile(webFile);
+
+        Assert.assertNotNull(webFile.getFileContent());
+        Assert.assertTrue(webFile.getFileContent().contains("<!DOCTYPE html>"));
+    }
+
+}
